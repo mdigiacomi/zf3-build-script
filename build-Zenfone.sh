fa70f14d22717a86ed85f25b974b63f28cf3ea62
Michael Di Giacomi <digitaladrenalin@gmail.com>
2018-02-14 16:23:15 -0500

    Update branches

--- build-Zenfone.sh
+++ build-Zenfone.sh
@@ -18,9 +18,9 @@ repo sync -j 32
 
 # Cloning Zenfone Resources
 
-git clone https://gitlab.com/LineageOS-Zenfone3/device_asus_14.2020.1712.85.git -b cm-14.1 /home/android/device/asus/Z017/
-git clone https://gitlab.com/LineageOS-Zenfone3/vendor_asus_14.2020.1712.85.git -b N /home/android/vendor/asus/Z017/
-git clone https://gitlab.com/LineageOS-Zenfone3/kernel_asus_14.2020.1712.85.git -b N /home/android/kernel/asus/Z017/
+git clone https://gitlab.com/LineageOS-Zenfone3/device_asus_14.2020.1712.85.git -b master /home/android/device/asus/Z017/
+git clone https://gitlab.com/LineageOS-Zenfone3/vendor_asus_14.2020.1712.85.git -b master /home/android/vendor/asus/Z017/
+git clone https://gitlab.com/LineageOS-Zenfone3/kernel_asus_14.2020.1712.85.git -b master /home/android/kernel/asus/Z017/
 
 # Cloning Dependencies
 
